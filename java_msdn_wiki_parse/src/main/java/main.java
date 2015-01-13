84b5c861889048213a2127ff31804ad5640c54d4
rsamokhin <poshliemail@googlemail.com>
2015-01-13 14:42:13 +0300

    one thread title tree creation

--- java_msdn_wiki_parse/src/main/java/main.java
+++ java_msdn_wiki_parse/src/main/java/main.java
@@ -4,14 +4,18 @@ import java.io.PrintWriter;
 import java.net.URL;
 import java.net.MalformedURLException;
 import java.net.URLConnection;
+import java.util.HashMap;
 
 import org.json.*;
 
 public class main{
-    public static void main (String[]args)throws MalformedURLException, IOException{
+    public static void main (String[]args) throws IOException{
         Scanner in = new Scanner(System.in);
-        PrintWriter out = new PrintWriter(System.out);
         String surl = in.nextLine()+"?toc=1";
+        getJsonArray(surl,0,"HOME");
+    }
+    private static void getJsonArray(String surl,int depth,String title) throws MalformedURLException, IOException{
+        PrintWriter out = new PrintWriter(System.out);
         URL url = new URL(surl);
         URLConnection conn = url.openConnection();
         Scanner sFromUrl = new Scanner(conn.getInputStream());
@@ -19,9 +23,20 @@ public class main{
         while(sFromUrl.hasNextLine()){
             resultString.append(sFromUrl.nextLine());
         }
-	JSONArray jsonArray = new JSONArray(resultString.toString());
-        
-        
+        for (int i = 0 ; i < depth ; i++)
+            out.print("  ");
+        out.println(title);
+        try{
+            JSONArray jsonArray = new JSONArray(resultString.toString());
+            depth = depth+1;
+            for(int i = 0 ; i < jsonArray.length(); i++){
+                String newUrl,newTitle;
+                JSONObject json =  (JSONObject) jsonArray.get(i);
+                newUrl = "http://technet.microsoft.com"+(String) json.get("Href")+"?toc=1";
+                newTitle = (String) json.get("Title");
+                getJsonArray(newUrl,depth,newTitle);
+            }
+        }catch(JSONException | IOException e){}
         out.flush();
     }
 }
