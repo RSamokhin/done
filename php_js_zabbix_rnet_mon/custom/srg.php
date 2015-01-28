224c2a91714a80420ba8ec0ac5c6fb2702d4c3d2
rsamokhin <poshliemail@googlemail.com>
2015-01-28 17:26:12 +0300

    path to requeire changed

--- php_js_zabbix_rnet_mon/custom/srg.php
+++ php_js_zabbix_rnet_mon/custom/srg.php
@@ -1,7 +1,7 @@
 <?php
 
-require dirname(__FILE__).'../ZabbixApiAbstract.class.php';
-require dirname(__FILE__).'../ZabbixApi.class.php';
+require dirname(__FILE__).'/../ZabbixApiAbstract.class.php';
+require dirname(__FILE__).'/../ZabbixApi.class.php';
 
 try {
     $forquery = array();
