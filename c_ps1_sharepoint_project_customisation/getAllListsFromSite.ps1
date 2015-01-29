e6d86544736a64f152f4e72948cba1f2e1d630aa
rsamokhin <poshliemail@googlemail.com>
2015-01-29 08:19:32 -0800

    get all lists from site function added

---
+++ c_ps1_sharepoint_project_customisation/getAllListsFromSite.ps1
@@ -0,0 +1,14 @@
+﻿function getAllLists($site){
+    $array = @()
+    foreach ($web in $site.AllWebs) { 
+        foreach ($list in $web.lists){
+            $array += @{
+                siteUrl="http://pelmen-tg-prj/PWA";
+                webTitle = $web.Title;
+                listTitle = $list.Title;
+            }
+
+        } 
+    } 
+    return $array
+}
